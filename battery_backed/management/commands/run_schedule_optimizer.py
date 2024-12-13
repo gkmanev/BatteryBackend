1c47cc2fc02902af167167af60c222d240077184
Georgi <georgi.manev@entra.energy>
2024-12-13 10:35:06 +0200

    create sched

--- battery_backed/management/commands/run_schedule_optimizer.py
+++ battery_backed/management/commands/run_schedule_optimizer.py
@@ -12,15 +12,16 @@ class Command(BaseCommand):
         soc=0
         for row in optimized_schedule.itertuples():            
             
-            invertor = row.schedule          
-            flow = invertor/60
+            invertor = row.schedule
+            flow = invertor/60*15          
+            flow_min = invertor/60
             soc += flow
-                        
+
             BatteryLiveStatus.objects.get_or_create(
                     devId='batt-0001', 
                     timestamp=row.Index,
                     invertor_power=invertor,
-                    flow_last_min=flow,
+                    flow_last_min=flow_min,
                     state_of_charge=soc
                 )                    
 
