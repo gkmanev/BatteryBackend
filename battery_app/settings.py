5e83c3137bffd44abcb093f32ea500dcfbe0dc63
Georgi <georgi.manev@entra.energy>
2025-12-10 11:49:12 +0200

    beat

--- battery_app/settings.py
+++ battery_app/settings.py
@@ -160,9 +160,9 @@ CACHES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': 'db',
         'PORT': 5432,
     }
