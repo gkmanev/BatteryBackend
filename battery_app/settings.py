4089640affbd205d9a1b6ecb4048f0343e38e461
Georgi <georgi.manev@entra.energy>
2025-12-10 15:00:12 +0200

    beat

--- battery_app/settings.py
+++ battery_app/settings.py
@@ -118,7 +118,7 @@ CELERY_RESULT_BACKEND = os.getenv("CELERY_BACKEND", "redis://redis:6379/0")
 CELERY_BEAT_SCHEDULE = {
     "fetch_prices_afternoon": {
         "task": "battery_backed.tasks.task_fetch_prices",
-        "schedule": crontab(minute=53, hour=14),
+        "schedule": crontab(minute=2, hour=15),
     },
     "fetch_prices_evening": {
         "task": "battery_backed.tasks.task_fetch_prices",
