f0c56f45cb5f83b8a63adbb35838af4ab53b282c
gkmanev <64841915+gkmanev@users.noreply.github.com>
2026-01-19 13:36:30 +0200

    Remove JWT auth configuration

--- battery_backed/urls.py
+++ battery_backed/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from rest_framework import routers
 from .views import StateViewSet, ScheduleViewSet, BatteryCumulativeDataView, ScheduleCumulativeDataView, PopulateBatteryScheduleView, AggregateYearDataView, CumulativeYearDataView, PriceView, ForecastedPriceView, AccumulatedFlowPriceView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'state_of_charge', StateViewSet)
 router.register(r'schedule', ScheduleViewSet)
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
     path('battery-cumulative/', BatteryCumulativeDataView.as_view(), name='battery-cumulative'),
     path('schedule-cumulative/', ScheduleCumulativeDataView.as_view(), name='schedule-cumulative'), 
     path('populate-schedule/', PopulateBatteryScheduleView.as_view(), name='populate_schedule'),   
